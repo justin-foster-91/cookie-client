6f2b6d86bbbceb2530d6a926ca50b0154b99c640
Justin Foster <jfoster91@gmail.com>
2020-12-03 12:33:18 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [cookieClicks, setCookieClicks] = useState(0);
+
+  const addCount = () => {
+    setCookieClicks(cookieClicks + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="cookieDisplay">
+        <h3>{cookieClicks} cookies collected</h3>
+        <button 
+          onClick={() => addCount()}
+        >Not A Cookie</button>
+      </section>
+    </main>
   );
 }
 
